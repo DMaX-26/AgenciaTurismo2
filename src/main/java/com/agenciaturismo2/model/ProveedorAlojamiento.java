2f2d3f13c5bbfa40fd4cc7e9398fc9642227ef5a
Max <ma.alveare@duocuc.cl>
2026-06-26 09:30:33 -0400

    Correccion parametro metodo setNombre

--- src/main/java/com/agenciaturismo2/model/ProveedorAlojamiento.java
+++ src/main/java/com/agenciaturismo2/model/ProveedorAlojamiento.java
@@ -35,7 +35,7 @@ public class ProveedorAlojamiento {
     public String getNombre() {
         return nombre;
     }
-    public void setNombre(String nombrel) {
+    public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
